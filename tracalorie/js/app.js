d4852a5860c9018b1f37ed7ac7d6eeec32d50e2e
scottyTwoskip <scottyeansor@gmail.com>
2024-04-02 09:55:35 -0600

    added local storage functionallity

--- tracalorie/js/app.js
+++ tracalorie/js/app.js
@@ -1,6 +1,6 @@
 class CalorieTracker {
     constructor() {
-        this._calorieLimit = 2000
+        this._calorieLimit = Storage.getCalorieLimit()
         this._totalCalories = 0
         this._meals = []
         this._workouts = []
@@ -53,6 +53,7 @@ class CalorieTracker {
     }
     setLimit(calorieLimit) {
         this._calorieLimit = calorieLimit
+        Storage.setCalorieLimit(calorieLimit)
         this._displayCaloriesLimit()
         this._render()
     }
@@ -165,7 +166,20 @@ class Workout {
         this.calories = calories
     }
 }
-
+class Storage {
+    static getCalorieLimit(defaultLimit = 2000) {
+        let calorieLimit
+        if (localStorage.getItem('calorieLimit') === null) {
+            calorieLimit = defaultLimit
+        } else {
+            calorieLimit = +localStorage.getItem('calorieLimit')
+        }
+        return calorieLimit
+    }
+    static setCalorieLimit(calorieLimit) {
+        localStorage.setItem('calorieLimit', calorieLimit)
+    }
+}
 class App {
     constructor() {
         this._tracker = new CalorieTracker()
